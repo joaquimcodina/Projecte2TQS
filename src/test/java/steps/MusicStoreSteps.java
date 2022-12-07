d2b43e2866aef375d4bd8257c690970303ba7740
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-12-07 08:58:53 +0100

    Scenario DrumBass Category

--- src/test/java/steps/MusicStoreSteps.java
+++ src/test/java/steps/MusicStoreSteps.java
@@ -151,6 +151,11 @@ public class MusicStoreSteps {
     driver.findElement(By.cssSelector("#categorias-home a:nth-of-type(12)")).click();
   }
 
+  @When("the user selects drumbass category")
+  public void theUserSelectsDrumbassCategory() {
+    driver.findElement(By.cssSelector("#categorias-home a:nth-of-type(13)")).click();
+  }
+
   @When("the user adds a vinyl of the selected category to the cart")
   public void theUserAddsAVinylOfTheSelectedCategoryToTheCart() {
     WebElement element1 = driver.findElement(By.cssSelector("tr:nth-of-type(1) td:nth-of-type(1) a:nth-of-type(2)"));
