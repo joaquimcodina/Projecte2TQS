3ea7183eeaf0aaf12cf07b0170ad9a7fd31320fa
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-12-07 08:56:47 +0100

    Scenario Techno Category

--- src/test/java/steps/MusicStoreSteps.java
+++ src/test/java/steps/MusicStoreSteps.java
@@ -141,6 +141,11 @@ public class MusicStoreSteps {
     driver.findElement(By.cssSelector("#categorias-home a:nth-of-type(10)")).click();
   }
 
+  @When("the user selects techno category")
+  public void theUserSelectsTechnoCategory() {
+    driver.findElement(By.cssSelector("#categorias-home a:nth-of-type(11)")).click();
+  }
+
   @When("the user adds a vinyl of the selected category to the cart")
   public void theUserAddsAVinylOfTheSelectedCategoryToTheCart() {
     WebElement element1 = driver.findElement(By.cssSelector("tr:nth-of-type(1) td:nth-of-type(1) a:nth-of-type(2)"));
